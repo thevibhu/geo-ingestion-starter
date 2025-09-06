f769f4d84e9c6fcf19c5f04441119edf2ec367c9
Vaibhav Verma <thevaibhavverma@gmail.com>
2025-09-06 18:57:01 -0400

    models and alembic env fix

--- app/alembic/env.py
+++ app/alembic/env.py
@@ -7,19 +7,56 @@ config = context.config
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-from app.db import Base  # type: ignore
-from app.models import Feature, Footprint  # type: ignore
+from db import Base  # type: ignore
+from models import Feature, Footprint  # type: ignore
 target_metadata = Base.metadata
 
-def run_migrations_offline():
-    context.configure(url=os.getenv("DATABASE_URL"), target_metadata=target_metadata, literal_binds=True)
+database_url = os.getenv("DATABASE_URL")
+if database_url:
+    config.set_main_option("sqlalchemy.url", database_url)
+
+def run_migrations_offline() -> None:
+    """Run migrations in 'offline' mode.
+
+    This configures the context with just a URL
+    and not an Engine, though an Engine is acceptable
+    here as well.  By skipping the Engine creation
+    we don't even need a DBAPI to be available.
+
+    Calls to context.execute() here emit the given string to the
+    script output.
+
+    """
+    url = config.get_main_option("sqlalchemy.url")
+    context.configure(
+        url=url,
+        target_metadata=target_metadata,
+        literal_binds=True,
+        dialect_opts={"paramstyle": "named"},
+    )
+
     with context.begin_transaction():
         context.run_migrations()
 
-def run_migrations_online():
-    connectable = engine_from_config(config.get_section(config.config_ini_section), prefix="", poolclass=pool.NullPool)
+
+def run_migrations_online() -> None:
+    """Run migrations in 'online' mode.
+
+    In this scenario we need to create an Engine
+    and associate a connection with the context.
+
+    """
+    connectable = engine_from_config(
+        config.get_section(config.config_ini_section, {}),
+        prefix="sqlalchemy.",
+        poolclass=pool.NullPool,
+    )
+
     with connectable.connect() as connection:
-        context.configure(connection=connection, target_metadata=target_metadata)
+        context.configure(
+            connection=connection, target_metadata=target_metadata
+        )
+
         with context.begin_transaction():
             context.run_migrations()
 
--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import String, Integer, DateTime, ForeignKey
+from sqlalchemy import String, Integer, DateTime, ForeignKey, Float
 from sqlalchemy.dialects.postgresql import UUID as PG_UUID
+from geoalchemy2 import Geography
 from datetime import datetime, timezone
 import uuid
 from db import Base
@@ -16,7 +17,10 @@ class Feature(Base):
     attempts: Mapped[int] = mapped_column(Integer, default=0, nullable=False)
     created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), default=now, nullable=False)
     updated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), default=now, onupdate=now, nullable=True)
+    geom: Mapped[Geography] = mapped_column(Geography('POINT', srid=4326), nullable=True)
 
 class Footprint(Base):
     __tablename__ = "footprints"
     feature_id: Mapped[uuid.UUID] = mapped_column(PG_UUID(as_uuid=True), ForeignKey("features.id", ondelete="CASCADE"), primary_key=True)
+    geom: Mapped[Geography] = mapped_column(Geography('POLYGON', srid=4326), nullable=True)
+    buffer_area_m2: Mapped[float] = mapped_column(Float, nullable=True)
